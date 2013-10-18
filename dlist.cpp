58957aae2f8d57a3a10dc4fd53724e7ac2b466b5
Henry Tang <henry.yu.tang@gmail.com>
2013-10-17 22:45:31 -0700

    need to change

--- dlist.cpp
+++ dlist.cpp
@@ -4,91 +4,61 @@ using namespace std;
 
 struct Node{
      int data;
-     Node *next;
-     Node *prev;
+     Node *left;
+     Node *right;
 };
 
 Node *gHeadptr = NULL;
 
-void addNodetoList(int d)
+void initDlist(int d)
+{
+     gHeadptr = new Node;
+     gHeadptr -> left = NULL;
+     gHeadptr -> right = NULL; 
+     gHeadptr -> data = d;
+}
+
+void insertLeftDlist(int d)
 {
      Node *newnode = new Node;
-     newnode-> data = d;
-     newnode-> next = NULL;
-
-     if (gHeadptr == NULL) {
-         gHeadptr = newnode;
-         return;   
-     } 
-     Node *cur = gHeadptr; 
-     while( cur -> next != NULL) {
-          cur = cur -> next;
-     }
-     cur-> next = newnode;
-     return; 
+     newnode -> left = NULL;
+     newnode -> right = gHeadptr;
+     gHeadptr -> left = newnode; 
+     newnode -> data = d;
 }
 
-void iterateList(void)
+void insertRightDlist(int d)
+{
+     Node *newnode = new Node;
+     newnode -> left = gHeadptr;
+     gHeadptr -> right = newnode;
+     newnode -> right = NULL;
+     newnode -> data  = d;
+}
+
+void transverlDlist(void)
 {
      Node *cur = gHeadptr;
-     cout << "Start list:[ ";
+     while(cur -> left)
+         cur = cur -> left;
      while(cur) {
-         cout << cur -> data << ' '; 
-         cur = cur -> next;
+        cout << cur->data <<',';
+        cur = cur -> right;
      }
-     cout << "]" << endl; 
-}
-
-int lenList(void)
-{
-    int count = 0;
-    Node *cur = gHeadptr;
-    while(cur){
-       cur = cur -> next;
-       count ++;
-    }
-    return count; 
-}
-
-bool deleteNodefromList(int d)
-{
-      Node *cur = gHeadptr;
-      while(cur->next) {
-          if ( cur-> next -> data == d ) {
-              Node *delNode = cur -> next;
-              cur-> next = delNode -> next;
-              delete delNode;
-              return true;
-           }
-          cur = cur->next;
-      }
-      return false;
-}
-
-Node * searchList(int d)
-{
-    Node *cur = gHeadptr;
-    while(cur){
-        if (cur -> data == d) {
-           return cur;
-        }
-        cur = cur -> next; 
-    }
-    return NULL;    
+     
 }
 
 int main(void)
 {
-     for ( int i = 100; i< 120; i ++ )
-     addNodetoList(i);
-     cout << "length is:" << lenList() << endl;
-     iterateList();
-     deleteNodefromList(110);
-     deleteNodefromList(108);
-     iterateList();
-     cout << searchList(150) << endl;
-     cout << " End test " << endl;
+    initDlist(0);
+    for ( int i = 10; i< 20; i++)
+    {
+        insertLeftDlist(i);
+        insertRightDlist(i+1);
+    }
+    transverlDlist();
 }
+
 
 
 
