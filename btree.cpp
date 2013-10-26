5f031cd7103dc487a406e96e7eeb7bf41d55b212
Henry Tang <henry.yu.tang@gmail.com>
2013-10-25 20:51:03 -0700

    modified  btree.cpp

--- btree.cpp
+++ btree.cpp
@@ -16,9 +16,28 @@ Node * insert(Node *node, int d)
         return node;
      }
 
-     if ( d <= node -> data ) node ->left = insert( node->left, data);
-     else  node -> right = insert( node->right, data);
-     return (node);
-     
-     
+     if ( d <= node -> data ) node ->left = insert( node->left, d);
+     else  node -> right = insert( node->right, d);
 }
+
+void printTree( Node* node) { 
+  if (node == NULL) return;
+  printTree(node->left); 
+  cout << node->data <<"L:";
+  printTree(node->right); 
+  cout <<"R:"<< node->data;
+} 
+
+
+
+int main(void)
+{
+    Node *root = insert(NULL, 5);
+    Node *leaf = root;
+    
+    for ( int i = 1; i< 10; i++) {
+      leaf = insert(leaf,i);
+    }
+    printTree(root);
+}
+
